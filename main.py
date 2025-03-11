c0f30f591ef71f78ecc6958f32d998c6dadb90a5
yann <yann.rolland@esme.fr>
2025-03-11 14:53:00 +0100

    modif

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ def tell_joke(lang="en"):
 	print(pyjokes.get_joke(language=lang))
 
 if __name__ == "__main__":
-	lang = input("Choisissez une langue (en, de, es, it, gl): ")
+	lang = input("Choisissez une langues (en, de, es, it, gl): ")
 	joke_type= input("choisissez une catégorie de blagues (neutral, chuck, all): ")
 	tell_joke(lang, joke_type)
